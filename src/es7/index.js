2972da4261689584ea6e09a3203e7f872d4e7735
EberNar <eberrdz679@gmail.com>
2022-06-25 17:59:47 -0500

    Caracteristicaz agregadas con ECMAScript 7

---
+++ src/es7/index.js
@@ -0,0 +1,14 @@
+// Includes
+let numbers = [1, 2, 3, 4, 5];
+
+if (numbers.includes(3)){
+    console.log("si se encuentra el valor 3");
+}else{
+    console.log("No se encuentra");
+}
+
+// Exponente
+let base = 4;
+let exponent = 3;
+let result = base ** exponent;
+console.log(`El resultado de ${base} a la ${exponent} es igual a ${result}`);
